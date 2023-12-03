25d43a54704cf01b33ecc5094727044d6fa4231d
ThatBoiWitZm <152670919+ThatBoiWitZm@users.noreply.github.com>
2023-12-03 12:41:12 +0000

    Add files via upload

---
+++ CTF.py
@@ -0,0 +1,78 @@
+# Import necessary libraries
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+# Initialize Flask app and configure database
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ctf.db'
+db = SQLAlchemy(app)
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# User model for database
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+# Challenge model for database
+class Challenge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+    flag = db.Column(db.String(100), nullable=False)
+
+# Flask-Login user loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    challenges = Challenge.query.all()
+    return render_template('dashboard.html', challenges=challenges)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    # Create the database tables
+    db.create_all()
+
+    # Add some example challenges
+    example_challenge = Challenge(title='Example Challenge', description='This is an example challenge.', points=100, flag='example_flag')
+    db.session.add(example_challenge)
+    db.session.commit()
+
+    # Run the Flask app
+    app.run(debug=True)
